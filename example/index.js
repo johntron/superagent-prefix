e454b0fd4d7a326e44d707036b6927481aeb21b3
Andrew Silluron <asilluron@users.noreply.github.com>
2015-11-12 16:09:29 -0800

    Add semistandard style

--- example/index.js
+++ example/index.js
@@ -1,16 +1,16 @@
-var request = require('superagent'),
-	prefix = require('superagent-prefix')('static');
+var request = require('superagent');
+var prefix = require('superagent-prefix')('static');
 
 // Get static/data.json
 request.get('/data.json')
-	.use(prefix)
-	.end(function () {
-		console.log(arguments);
-	});
+  .use(prefix)
+  .end(function () {
+    console.log(arguments);
+  });
 
 // Get static/other-data.json
 request.get('/other-data.json')
-	.use(prefix)
-	.end(function () {
-		console.log(arguments);
-	});
+  .use(prefix)
+  .end(function () {
+    console.log(arguments);
+  });
